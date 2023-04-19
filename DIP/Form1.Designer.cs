b441ac87ea8cc944f7b1ac43937aa39620d82fe1
Steven Lim <stevenlimofficial@gmail.com>
2023-04-19 13:47:46 +0700

    init
    init

---
+++ DIP/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace DIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.histogramBox1 = new Emgu.CV.UI.HistogramBox();
+            this.stop = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(9, 26);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(320, 240);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(145, 294);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(54, 17);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Binary";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(662, 350);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // histogramBox1
+            // 
+            this.histogramBox1.Location = new System.Drawing.Point(719, 9);
+            this.histogramBox1.Name = "histogramBox1";
+            this.histogramBox1.Size = new System.Drawing.Size(283, 240);
+            this.histogramBox1.TabIndex = 13;
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(10, 318);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 14;
+            this.stop.Text = "Shoot";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Location = new System.Drawing.Point(10, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Choose";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Compare";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(479, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(794, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(648, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(455, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(109, 377);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(335, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 240);
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(458, 348);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(198, 45);
+            this.trackBar3.TabIndex = 29;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(308, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Matching Percentage";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(423, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 31;
+            this.label7.Text = ":";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(980, 20);
+            this.textBox2.TabIndex = 32;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(651, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(310, 240);
+            this.pictureBox2.TabIndex = 33;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(308, 353);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 13);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "Tolerance Percentage";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(423, 352);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 13);
+            this.label9.TabIndex = 35;
+            this.label9.Text = ":";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(455, 314);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 36;
+            this.label10.Text = "label10";
+            this.label10.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 405);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.histogramBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ DIP/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace DIP
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string gg;
+        int max, min;
+
+        Capture capture = new Capture();
+        Image<Bgr, byte> frame= new Image<Bgr,byte>(320,240);
+        Image<Bgr, byte> frameR = new Image<Bgr, byte>(320, 240);
+        Image<Bgr, byte> frameG = new Image<Bgr, byte>(320, 240);
+        Image<Bgr, byte> frameB = new Image<Bgr, byte>(320, 240);
+        Image<Gray, byte> lightness = new Image<Gray, byte>(320, 240);        
+        Image<Gray, byte> average = new Image<Gray, byte>(320, 240);
+        Image<Gray, byte> luminosity = new Image<Gray, byte>(320, 240);
+        Image<Gray, byte> binary = new Image<Gray, byte>(320, 240);
+
+        double whiteColor = 0;
+        double blackColor = 0;
+        double whiteColor2 = 0;
+        double blackColor2 = 0;
+        double a = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton4.Checked = true;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            frame = capture.QueryFrame().ToImage<Bgr, byte>();
+            frame = frame.Resize(320, 240, Emgu.CV.CvEnum.Inter.Cubic).Flip(Emgu.CV.CvEnum.FlipType.Horizontal);
+
+
+          
+
+            if (radioButton4.Checked) 
+            {
+                
+                for (int x = 0; x < frame.Height; x++)
+                {
+                    for (int y = 0; y < frame.Width; y++)
+                    {
+                        byte R = frame.Data[x, y, 2];
+                        byte G = frame.Data[x, y, 1];
+                        byte B = frame.Data[x, y, 0];
+                        average.Data[x, y, 0] = Convert.ToByte((R + G + B) / 3);
+                    }
+                }
+                for (int x = 0; x < frame.Height; x++)
+                {
+                    for (int y = 0; y < frame.Width; y++)
+                    {
+                        if (average.Data[x, y, 0] < 240)
+                        {
+                            binary.Data[x, y, 0] = 0;
+                        }
+                        else
+                        {
+                            binary.Data[x, y, 0] = 255;
+                        }
+                    }                    
+                    imageBox2.Image=binary;
+                    binary.Save(gg);
+                    //histogramBox1.Visible = false;
+                }                            
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = binary.ToBitmap();
+            binary.Save(@"D:\MyPic3.jpg");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    pictureBox2.ImageLocation = dialog.FileName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(pictureBox2.Image);
+            try
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        Color color = bmp.GetPixel(x, y);
+
+                        if (color.ToArgb() == Color.White.ToArgb())
+                        {
+                            whiteColor++;
+                        }
+
+                        else
+                        if (color.ToArgb() == Color.Black.ToArgb())
+                        {
+                            blackColor++;
+                        }
+                    }
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label1.Text = Convert.ToString(whiteColor ) + " white pixels";
+                    label2.Text = Convert.ToString(blackColor ) + " black pixels";
+                }
+
+                {
+                    for (int x = 0; x < bmp2.Width; x++)
+                    {
+                        for (int y = 0; y < bmp2.Height; y++)
+                        {
+                            Color color = bmp2.GetPixel(x, y);
+
+                            if (color.ToArgb() == Color.White.ToArgb())
+                            {
+                                whiteColor2++;
+                            }
+
+                            else
+                            if (color.ToArgb() == Color.Black.ToArgb())
+                            {
+                                blackColor2++;
+                            }
+                        }
+                        label3.Visible = true;
+                        label4.Visible = true;
+                        label3.Text = Convert.ToString(blackColor2) + " black pixels";
+                        label4.Text = Convert.ToString(whiteColor2) + " white pixels";
+                    }
+                }
+                
+                this.Cursor = Cursors.WaitCursor;
+                a = Math.Round(((whiteColor / whiteColor2) * 100), 2 );
+
+                if (a >= 0 && a <= 100)
+                {
+                    label5.Text = Convert.ToString(a);
+                    label5.Visible = true;
+                    label10.Visible = false;
+                }
+
+                else if ( a >= 100 && a <= 103 )
+                {
+                    a = 100;
+                    label5.Text = Convert.ToString(a);
+                    label5.Visible = true;
+                    label10.Visible = false;
+                }
+
+                else if (a >= 104)
+                {
+                    label5.Visible = false;
+                    label10.Text = "OUT OF RANGE";
+                    label10.Visible = true;
+                }
+
+                if (a >= trackBar3.Value && a <= 103)
+                {
+                    textBox2.Text = "PASS";
+                    textBox2.BackColor = Color.Green;
+                    textBox2.ForeColor = Color.White;
+                    blackColor = 0;
+                    blackColor2 = 0;
+                    whiteColor = 0;
+                    whiteColor2 = 0;
+
+                }
+                else
+                {
+                    textBox2.Text = "FAIL";
+                    textBox2.BackColor = Color.Red;
+                    textBox2.ForeColor = Color.Black;
+                    blackColor = 0;
+                    blackColor2 = 0;
+                    whiteColor = 0;
+                    whiteColor2 = 0;
+                }
+
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.InnerException.Message); }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar3.Value.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            binary.Save(@"D:\MyPic.bmp");
+        }
+
+        
+
+         
+    }
+}
